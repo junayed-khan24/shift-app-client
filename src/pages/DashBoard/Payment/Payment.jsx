1545b455800d88ff71f3a4b5423e1ba141fe0e6d
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-01-25 23:58:29 +0600

    added code

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Payment = () => {
     return (
         <div>
-            <h2 className="text-4xl text-primary">Payment Payment</h2>
+            <h2 className="text-4xl text-primary md:text-8xl">Payment Payment</h2>
         </div>
     );
 };
